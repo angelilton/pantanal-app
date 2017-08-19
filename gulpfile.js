4e5ebf2ba404384fb42ccd35bd99fee956012f15
angelilton Epifanio <epifanio.rds@gmail.com>
2017-08-19 02:23:10 -0400

    aula de estrutura do projeto e automatizando com gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp')
+var pug = require('gulp-pug')
+var stylus = require('gulp-stylus')
+
+gulp.task('pug', function(){
+    gulp.src('./src/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./out'))
+})
+
+gulp.task('stylus', function () {
+    gulp.src('./src/assets/styles/*.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./out/assets/styles/'))
+})
+
+gulp.task('watch', function(){
+    gulp.watch(['./src/*.pug'],['pug'])
+    gulp.watch(['./src/assets/styles/*.styl'],['stylus'])
+
+})
+
+
+gulp.task('build',['pug','stylus'])
+
